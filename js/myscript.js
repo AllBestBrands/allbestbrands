1414dfd66401f6422b41b6b42ce53a138dd4902e
AllBestBrands <allbestbrandsbotswana@gmail.com>
2016-05-02 15:49:22 +0200

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,46 +1,45 @@
- (function(){ 
-   var ref = new Firebase("https://allbestbrands.firebaseio.com");
-  
-  window.fbAsyncInit = function() {
-      FB.init({
-          appId      : '960632784021692',
-          xfbml      : true,
-          version    : 'v2.6'
-      });
-  };
 
-  (function(d, s, id){
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {return;}
-      js = d.createElement(s); js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+  var ref = new Firebase("https://allbestbrands.firebaseio.com");
+ 
+ window.fbAsyncInit = function() {
+     FB.init({
+         appId      : '960632784021692',
+         xfbml      : true,
+         version    : 'v2.6'
+     });
+ };
 
-  $("#loginWithFacebookButton").click(function(){
-      ref.authWithOAuthPopup("facebook", function(error, authData) { 
-          
-          if (error) {
-              console.log("Login Failed!", error);
-          } else {
-              // console.log("Authenticated successfully with payload:", authData);
-              // $("#login").css("display", "none");
-              // $("#loginWithFacebookButton").css("display", "none");
-              // $("#loggedIn").css("display", "block");
-              // console.log(authData);
-              if (authData.facebook.id==1000231920018619){
-                  var userAuthData = authData;
-                  window.location="/post.html"
-              }
-              else
-              {
-                  alert("Not authorised");
-              }
-          }
+ (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
 
-      }, {
-          remember: "none",
-          scope: "email,user_likes"
-      });
-  });
+ $("#loginWithFacebookButton").click(function(){
+     ref.authWithOAuthPopup("facebook", function(error, authData) { 
+         
+         if (error) {
+             console.log("Login Failed!", error);
+         } else {
+             // console.log("Authenticated successfully with payload:", authData);
+             // $("#login").css("display", "none");
+             // $("#loginWithFacebookButton").css("display", "none");
+             // $("#loggedIn").css("display", "block");
+             // console.log(authData);
+             if (authData.facebook.id==1000231920018619){
+                 var userAuthData = authData;
+                 window.location="/post.html"
+             }
+             else
+             {
+                 alert("Not authorised");
+             }
+         }
+
+     }, {
+         remember: "none",
+         scope: "email,user_likes"
+     });
  });
