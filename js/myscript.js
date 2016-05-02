61ad7959f45d3e5e4eadb5de0521bc58bba47b8d
AllBestBrands <allbestbrandsbotswana@gmail.com>
2016-05-02 14:43:56 +0200

    Create myscript.js

---
+++ js/myscript.js
@@ -0,0 +1,44 @@
+  var ref = new Firebase("https://allbestbrands.firebaseio.com");
+  
+  window.fbAsyncInit = function() {
+      FB.init({
+          appId      : '960632784021692',
+          xfbml      : true,
+          version    : 'v2.6'
+      });
+  };
+
+  (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  $("#loginWithFacebookButton").click(function(){
+      ref.authWithOAuthPopup("facebook", function(error, authData) { 
+          
+          if (error) {
+              console.log("Login Failed!", error);
+          } else {
+              // console.log("Authenticated successfully with payload:", authData);
+              // $("#login").css("display", "none");
+              // $("#loginWithFacebookButton").css("display", "none");
+              // $("#loggedIn").css("display", "block");
+              // console.log(authData);
+              if (authData.facebook.id==1000231920018619){
+                  window.location="/post.html"
+              }
+              else
+              {
+                  alert("Not authorised");
+              }
+          }
+
+      }, {
+          remember: "sessionOnly",
+          scope: "email,user_likes"
+      });
+  });
+  
